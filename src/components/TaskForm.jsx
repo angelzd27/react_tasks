cc2e33660743e6c7aaf4e2cd9105728d9fe2af02
angelzd27 <angeltj27@gmail.com>
2023-03-19 17:33:53 -0600

    rounded

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -18,16 +18,16 @@ function TaskForm() {
 
     return (
         <div className="max-w-md mx-auto">
-            <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+            <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4 rounded-md">
                 <h1 className="text-2xl font-bold text-white mb-3">Create your task</h1>
                 <input placeholder="Write your homework"
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
-                className="bg-slate-300 p-3 w-full mb-2"
+                className="bg-gray-500 p-3 w-full mb-2"
                 autoFocus/>
                 <textarea placeholder="Write your description of task"
                 onChange={(e) => setDescription(e.target.value)}
-                className="bg-slate-300 p-3 w-full mb-2"
+                className="bg-gray-500 p-3 w-full mb-2"
                 value={description}></textarea>
                 <button className="bg-indigo-500 px-3 py-1 rounded-md text-white hover:bg-indigo-400">Save</button>
             </form>
